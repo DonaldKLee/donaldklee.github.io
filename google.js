6d6fc5096e5ede8544c0f6d704334a43b728b414
DonaldKLee <Donald3010@outlook.com>
2022-04-01 22:58:00 -0700

    Completed Google Memes v1

--- google.js
+++ google.js
@@ -1,19 +1,55 @@
-function spawn_duck() {
-    
-}
-
 function spawn_meme() {
+    var meme_container = document.getElementById("meme_container");
+    var meme = document.createElement("img");
 
+    meme.classList.add("meme");
+
+    const meme_images = [
+        "googleassets/eggdog.gif",
+        "https://static.wikia.nocookie.net/smash_moveset_fanon/images/b/b1/Chrome-dinosaur.png/revision/latest/top-crop/width/360/height/450?cb=20201214102640",
+        "https://static.wikia.nocookie.net/ducksong/images/5/55/DuckFullBody.png/revision/latest/scale-to-width-down/276?cb=20211223200904",
+    ];
+    const image = Math.floor(Math.random() * meme_images.length);
+    meme.src = meme_images[image]; 
+
+    if (Math.random() < 0.5) { // 50% chance of the shark moving from right to left
+        meme.classList.add("right_meme");
+    }
+    else {
+        meme.classList.add("left_meme");
+    }
+
+    if (Math.random() < 0.1) { // 20% chance of a rotating meme
+        meme.classList.add("left_rotate_meme");
+    }
+    else if (0.1 < Math.random() < 0.2) { // 20% chance of a rotating meme
+        meme.classList.add("right_rotate_meme");
+    }
+
+    // If the shark isn't captured
+    meme.addEventListener("animationend", function() {
+        this.remove();
+    });
+
+    meme_container.appendChild(meme);
 }
 
+function lucky() {
+    for (let i=0; i<10; i++) {
+        spawn_meme();
+        spawn_meme();
+        spawn_meme();
+    }
+}
 
+function language() {
+    window.location.href='https://translate.google.ca/?sl=en&tl=fr&text=Rick%20Roll&op=translate';
 
-function rickroll_clicked() {
-    window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
 }
 
 function google_search() {
     search = document.getElementById("search_bar").value;
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
     window.location.href="https://www.google.com/search?q=" + search;
 }
 
@@ -32,4 +68,4 @@ document.getElementById("search_bar").addEventListener("keyup",
 
 setInterval(function(){ 
     spawn_meme();
-}, 1000);
+}, 500);
